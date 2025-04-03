634816df3f1bcc9b0d4e3d41dd8df4390c16f348
HalenaAquino <113187217+HalenaAquino@users.noreply.github.com>
2025-04-03 11:53:34 -0500

    Create About.js

---
+++ src/About.js
@@ -0,0 +1,12 @@
+    function About() {
+                                return (
+                                  <div>
+                                    <h1>About Us</h1>
+                                    <p>We are the ShopSmart Solutions cybersecurity team. Our job is to to protect their
+                                    digital assets by identifying, preventing, and responding to any and all cyber threats,
+                                    ensuring data security, and maintaining compliance with relevant regulations</p>
+                                  </div>
+                                );
+                              }
+
+                              export default About;
