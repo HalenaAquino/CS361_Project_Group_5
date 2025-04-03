a5aff63f2eab0559b38ea3761fb7f9b58856269f
HalenaAquino <113187217+HalenaAquino@users.noreply.github.com>
2025-04-03 11:54:27 -0500

    Update App.js
    Added "About" page

--- src/App.js
+++ src/App.js
@@ -11,12 +11,14 @@
                               <li><Link to="/">Home</Link></li>
                               <li><Link to="/dashboard">Dashboard</Link></li>
                               <li><Link to="/Login">Login</Link></li>
+                              <li><Link to="/About">About</Link></li>
                             </ul>
                           </nav>
                           <Routes>
                             <Route path="/" element={<Home />} />
                             <Route path="/login" element={<Login />} />
                             <Route path="/dashboard" element={<Dashboard />} />
+                            <Route path="/about" element={<About />} />
                           </Routes>
                          </div>
                       );
